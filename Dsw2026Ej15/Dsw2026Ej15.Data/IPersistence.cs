b4f6f30873dbebc0d3b796b92a1a364c8cce087b
Claudia Olea <claudiaolea6@gmail.com>
2026-06-16 17:43:05 -0300

    Creacion del proyecto, capas y clases

---
+++ Dsw2026Ej15/Dsw2026Ej15.Data/IPersistence.cs
@@ -0,0 +1,21 @@
+﻿using Dsw2026Ej15.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej15.Data
+{
+    public interface IPersistence
+    {
+        Task<Speciality?> GetSpecialityAsyncById(Guid id);
+
+        //Metodos
+        Task AddDoctorsAsync(Doctor doctor);
+        Task <IEnumerable<Doctor>> GetDoctorsAsync();
+        Task<Doctor?> GetDoctorAsyncById(Guid id);
+        Task<bool> DesactivateDoctorAsync(Guid id);
+
+
+
+    }
+}
