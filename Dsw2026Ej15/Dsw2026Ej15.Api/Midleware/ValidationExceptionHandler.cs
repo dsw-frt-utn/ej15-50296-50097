b68407031445bc4cf2e6874a1af9012930bdae87
Claudia Olea <claudiaolea6@gmail.com>
2026-06-19 11:23:17 -0300

    Se agregó el punto 3.h.ii(codigo 500) en la clase ValidationExceptionHandler

--- Dsw2026Ej15/Dsw2026Ej15.Api/Midleware/ValidationExceptionHandler.cs
+++ Dsw2026Ej15/Dsw2026Ej15.Api/Midleware/ValidationExceptionHandler.cs
@@ -2,25 +2,35 @@
 
 using Dsw2026Ej15.Api.Exceptions;
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 public class ValidationExceptionHandler : IExceptionHandler
 {
-    public async ValueTask<bool> TryHandleAsync(
-        HttpContext httpContext,
-        Exception exception,
-        CancellationToken cancellationToken)
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
     {
-        if (exception is not ValidationException validationException)
+        if (exception is ValidationException validationException)
         {
-            return false; // No es nuestro error, que siga buscando otro handler
+            httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            httpContext.Response.ContentType = "application/json";
+
+            var errorResponse = new { error = validationException.Message };
+            await httpContext.Response.WriteAsJsonAsync(errorResponse, cancellationToken);
+
+            return true; 
+
         }
+        httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        httpContext.Response.ContentType = "application/problem+json"; 
 
-        httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
-        httpContext.Response.ContentType = "application/json";
+        var problemDetails = new ProblemDetails
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Title = "Problem",
+            Detail = "Ha ocurrido un error interno inesperado en el servidor."
+        };
 
-        var errorResponse = new { error = validationException.Message };
-        await httpContext.Response.WriteAsJsonAsync(errorResponse, cancellationToken);
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
 
-        return true; // Excepción manejada exitosamente
+        return true; 
     }
 }
